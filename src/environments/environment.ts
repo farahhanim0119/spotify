a17082ea9504298ac446c7eb3de38903b1808b94
Farah Hanim <61750536+farahhanim0119@users.noreply.github.com>
2020-05-08 22:30:56 +0800

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  tokenType: "Bearer",
+  accessToken:
+  "BQD8N5i78jrZTmpkUqx64PK3KHa53CbrIzkeqZScWTeLGIwiYgmeahI-lnObzW0VeciNlhVXI09pio1Fqd4eZ-Uhg_kBEBqSswOic1wUGdkGQZmYZl3e5q22isY1E37hlhXpFJMjZF4Y",
+  baseUrl: "https://api.spotify.com/v1/"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
